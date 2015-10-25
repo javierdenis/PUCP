053b76facd93a75e4be69f2097bfe3070aeb027a
javier <javier@jvrdns>
2015-10-24 23:44:05 -0500

    JvrLptp-Ubuntu: AlgoritmoGenetico : mejora sobre la F.O, ahora se evalua entrada y salida a la interseccion de las vias del corredor vial

--- AlgoritmoGenetico/src/algoritmogenetico/AlgoritmoGenetico.java
+++ AlgoritmoGenetico/src/algoritmogenetico/AlgoritmoGenetico.java
@@ -14,21 +14,19 @@ import java.util.ArrayList;
 public class AlgoritmoGenetico {
     static Integer numIntersecciones = 10;
     static Integer tamPoblacion = 100; //que sea par->el porque esta en aqui1
-    static ArrayList<Indiv> p = new ArrayList<Indiv>();
-    static ArrayList<Indiv> np = new ArrayList<Indiv>();
-    static ArrayList<Indiv> ps = new ArrayList<Indiv>();
-    static Indiv hijo = new Indiv();
-    static Indiv hija = new Indiv();
+    static ArrayList<Individuo> p = new ArrayList<Individuo>();
+    static ArrayList<Individuo> np = new ArrayList<Individuo>();
+    static ArrayList<Individuo> ps = new ArrayList<Individuo>();
+    static Individuo hijo = new Individuo();
+    static Individuo hija = new Individuo();
 
     
-    public static class Indiv{
-        int IR;
+    public static class Individuo{
+        int IR; //Indice de Rendimiento
         ArrayList <Interseccion> intersecciones = new ArrayList<Interseccion>();
     
     }
-    public static class Interseccion{
-    
-    }
+  
     
 
     public static void main(String[] args) {
@@ -48,13 +46,13 @@ public class AlgoritmoGenetico {
                 np.add(hijo);
                 np.add(hija);
             }
-            SeleccionMejoresIndividuos();
+            SeleccionMejoresIndividuoiduos();
 //            if (PoblacionConverge()){
 //            }
         }
     }
-    public static void SeleccionMejoresIndividuos(){
-        ps=new ArrayList<Indiv>();
+    public static void SeleccionMejoresIndividuoiduos(){
+        ps=new ArrayList<Individuo>();
         for (int i=0; i<tamPoblacion;i++){
             if(p.get(i).IR<=np.get(i).IR){
                 ps.add(p.get(i));
@@ -66,7 +64,7 @@ public class AlgoritmoGenetico {
     public static void MutacionDeHijos(double probabilidad){
         
     }
-    public static void Casamiento(Indiv padre, Indiv madre){
+    public static void Casamiento(Individuo padre, Individuo madre){
         /*ind_hijo=*/ObtenerIndividuo();
         /*ind_hija=*/ObtenerIndividuo();
         //continuar con el casamiento;
@@ -85,17 +83,17 @@ public class AlgoritmoGenetico {
         return true;
     }
     public static void GenerarPoblacionInicial(){
-        Indiv ind ;
+        Individuo ind ;
         for (int i=0; i<tamPoblacion; i++){
-            ind = new Indiv();
+            ind = new Individuo();
             p.add(ind);
         }
     }
-    public static double CalcularIndiceRendimientoxIndividuo(Indiv ind){
+    public static double CalcularIndiceRendimientoxIndividuo(Individuo ind){
         double suma=0;
         double valor=0;
-        //Evaluamos para cada interseccion los datos que se obtienen alli
-        for (int i = 1; i <= numIntersecciones; i++) {
+        //Evaluamos para cada interseccion INTERCALADA los datos que se obtienen alli
+        for (int i = 0; i < numIntersecciones; i++) {
             valor+=EvaluarInterseccion(ind.intersecciones.get(i));
             System.out.println("Resultado FO = " + valor);
             suma+=valor;
@@ -103,13 +101,55 @@ public class AlgoritmoGenetico {
         return suma;
     }
     public static Double  EvaluarInterseccion(Interseccion intersecccion){
-        return FObjetivo(900, 10, 10, 200, 120,90);
+        double resultado=0;
+        
+        resultado+=FObjetivo2(intersecccion, 200, 120,90);
+        
+        return resultado;
     }
-    public static double FObjetivo(int q, int verde, int Ciclo_red, int S, int tao, int ciclo_semaforo) {
+    
+//    public static double FObjetivo(
+//            int q, /*flujo del arco*/
+//            int verde_efectivo, 
+//            int Ciclo_red /*dato fijo*/, 
+//            int S/*flujo saturacion, dato fijo*/,
+//            int tao,/*periodo de análisis 10-15-30 minutos*/ 
+//            int ciclo_semaforo) 
+//    {
+//        double DU = q/ciclo_semaforo;//promedio de longitudes de cola en todos los intervalos;
+//        int Q = verde_efectivo / Ciclo_red * S;
+//        int x = q / Q;
+//        double x0 = 0.67 + (S * verde_efectivo) / 600;
+//        double N = Q * tao / 4;
+//
+//        if (x > x0) {
+//            N = N * ((x - 1) + Math.sqrt(Math.pow(x - 1, 2) + (12 * (x - x0)) / (Q * tao)));
+//            return N;
+//        } else {
+//            return -1;
+//        }
+//    }
+    
+    
+    public static double FObjetivo2(
+            Interseccion i,
+            int Ciclo_red /*dato fijo*/, 
+            int S/*flujo saturacion, dato fijo*/,
+            int tao/*periodo de análisis 10-15-30 minutos*/ 
+            ) 
+    {
+        
+        //ESTO SOLO SIRVE PARA EVALUAR UNA AVENIDA QUE ENTRA A LA INTERSECCION
+        //FALTA EVALUAR EL OTRO SENTIDO;
+        int q=i.getQ_EO();
+        int ciclo_semaforo=i.getC1()+i.getC2()+i.getC3();
+        int verde_efectivo;
+        verde_efectivo=i.isB()?i.getC2():i.getC1()+i.getC3();
+        
         double DU = q/ciclo_semaforo;//promedio de longitudes de cola en todos los intervalos;
-        int Q = verde / Ciclo_red * S;
+        int Q = verde_efectivo / Ciclo_red * S;
         int x = q / Q;
-        double x0 = 0.67 + (S * verde) / 600;
+        double x0 = 0.67 + (S * verde_efectivo) / 600;
         double N = Q * tao / 4;
 
         if (x > x0) {
---
+++ AlgoritmoGenetico/src/algoritmogenetico/Interseccion.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmogenetico;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author javier
+ */
+public class Interseccion implements Runnable {
+    
+    //La duracion del primer color sera = c1+c3;
+    //La duracion del segundo color sera = c2;
+    //La duracion total del ciclo sera = c1+c2+c3+2*IGP(3segundos)
+
+    private String name;
+    private boolean b; // PRIMER COLOR: La interseccion inicia en verde = 1 orojo =0 para la vía principal
+    private int c1;
+    private int c2;
+    private int c3;
+    private int contador;
+    
+    private int q_EO, q_OE, q_NS, q_SN;
+    private int vm_EO, vm_OE, vm_NS, vm_SN;
+
+    public Interseccion(String name, boolean estado, int c1, int c2, int c3) {
+        this.name = name;
+        this.b = estado;
+        this.c1 = c1;
+        this.c2 = c2;
+        this.c3 = c3;
+        
+        this.q_EO = this.q_OE = this.q_NS = this.q_SN =100;
+        this.vm_EO= this.vm_OE= this.vm_NS= this.vm_SN=20;
+    }
+
+
+    @Override
+    public void run() {
+        while (true) {
+            contador = b ? c2 : c1;
+            while (contador >= 0) {
+                contador--;
+                try {
+                    Thread.sleep(1000); //duerme un segundo
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Interseccion.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+            b=!b;
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public boolean isB() {
+        return b;
+    }
+    public void setB(boolean b) {
+        this.b = b;
+    }
+    public int getC1() {
+        return c1;
+    }
+    public void setC1(int c1) {
+        this.c1 = c1;
+    }
+    public int getC2() {
+        return c2;
+    }
+    public void setC2(int c2) {
+        this.c2 = c2;
+    }
+    public int getContador() {
+        return contador;
+    }
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+
+    public int getC3() {
+        return c3;
+    }
+
+    public void setC3(int c3) {
+        this.c3 = c3;
+    }
+
+    public int getQ_EO() {
+        return q_EO;
+    }
+
+    public void setQ_EO(int q_EO) {
+        this.q_EO = q_EO;
+    }
+
+    public int getQ_OE() {
+        return q_OE;
+    }
+
+    public void setQ_OE(int q_OE) {
+        this.q_OE = q_OE;
+    }
+
+    public int getQ_NS() {
+        return q_NS;
+    }
+
+    public void setQ_NS(int q_NS) {
+        this.q_NS = q_NS;
+    }
+
+    public int getQ_SN() {
+        return q_SN;
+    }
+
+    public void setQ_SN(int q_SN) {
+        this.q_SN = q_SN;
+    }
+
+    public int getVm_EO() {
+        return vm_EO;
+    }
+
+    public void setVm_EO(int vm_EO) {
+        this.vm_EO = vm_EO;
+    }
+
+    public int getVm_OE() {
+        return vm_OE;
+    }
+
+    public void setVm_OE(int vm_OE) {
+        this.vm_OE = vm_OE;
+    }
+
+    public int getVm_NS() {
+        return vm_NS;
+    }
+
+    public void setVm_NS(int vm_NS) {
+        this.vm_NS = vm_NS;
+    }
+
+    public int getVm_SN() {
+        return vm_SN;
+    }
+
+    public void setVm_SN(int vm_SN) {
+        this.vm_SN = vm_SN;
+    }
+    
+    
+    
+}
